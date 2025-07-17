43dc19c4491631335c256185e1bd342ee9a5a31c
Peter Kin <kinpeter85@gmail.com>
2025-07-17 08:40:06 +0200

    Update config

--- app/common/config.py
+++ app/common/config.py
@@ -1,14 +1,11 @@
 allow_origins = [
-    "https://p-kin.com",
-    "https://www.p-kin.com",
     "https://api.p-kin.com",
     "https://start.p-kin.com",
     "https://startv4.p-kin.com",
     "https://tripz.p-kin.com",
     "https://stuff.p-kin.com",
     "https://rddit.p-kin.com",
-    "http://localhost",
-    "http://localhost:*",
-    "http://127.0.0.1",
-    "http://127.0.0.1:*",
+    "http://localhost:5100",  # tripz
+    "http://localhost:4444",  # startv4
+    "http://localhost:5499",  # pkrddit
 ]
