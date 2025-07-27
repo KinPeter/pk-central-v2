20daf4c22ed9e6745c9d009d3bee01f745ceff8b
Peter Kin <kinpeter85@gmail.com>
2025-07-27 13:07:10 +0200

    Add strava routes urls to config

--- app/common/config.py
+++ app/common/config.py
@@ -4,8 +4,10 @@ allow_origins = [
     "https://startv4.p-kin.com",
     "https://tripz.p-kin.com",
     "https://stuff.p-kin.com",
+    "https://strava.p-kin.com",
     "https://rddit.p-kin.com",
     "http://localhost:5100",  # tripz
     "http://localhost:4444",  # startv4
     "http://localhost:5499",  # pkrddit
+    "http://localhost:5498",  # stravarts
 ]
